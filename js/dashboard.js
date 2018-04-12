b76a01c1a9511105c66987de30aef58c1b41d0c0
Matthias-GOUDJIL <matthias.goudjil@gmail.com>
2018-04-12 21:55:08 +0200

    inial commit

---
+++ js/dashboard.js
@@ -0,0 +1,6 @@
+var btn_deconnexion=    document.getElementById('btn-deconnexion');
+
+  btn_deconnexion.addEventListener('click', function (e){
+    console.log("ya kinkin????");
+    window.location.href="index.html";
+  });
---
+++ js/script.js
@@ -0,0 +1,33 @@
+var btn_sign= document.getElementById('btn-signIn');
+
+var modal_sign_up=      document.getElementById('modal-signUp');
+var modal_sign_in=      document.getElementById('modal-signIn');
+var btn_inscription=    document.getElementById('btn-inscription');
+var btn_retour=         Array.from(document.getElementsByClassName('btn-retour'));
+var btn_submit=         Array.from(document.getElementsByClassName('btn-submit'));
+var btn_deconnexion=    document.getElementById('btn-deconnexion');
+
+btn_sign.addEventListener('click',  function () {
+    modal_sign_in.classList.toggle('hidepop');
+    if (!modal_sign_up.classList.contains('hidepop')) {
+        modal_sign_up.classList.add('hidepop');    
+    }
+});
+
+btn_inscription.addEventListener('click', function () {
+    modal_sign_up.classList.toggle('hidepop');
+    modal_sign_in.classList.toggle('hidepop');
+  });
+
+  btn_retour.forEach(function(item) {
+    item.addEventListener('click', function (e) {
+        window.location.reload();
+    });
+  });
+
+  btn_submit.forEach(function(item) {
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
+        window.location.href="dashboard.html";
+    });
+  });
