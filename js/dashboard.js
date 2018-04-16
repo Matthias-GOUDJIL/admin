b2b7f6ccd27f9575293500f362959293c1000e9d
Matthias-GOUDJIL <matthias.goudjil@gmail.com>
2018-04-16 16:55:46 +0200

    added index scss

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,9 +1,9 @@
-// var btn_deconnexion=    document.getElementById('btn-deconnexion');
+var btn_deconnexion= document.getElementById('btn-deconnexion');
 
-//   btn_deconnexion.addEventListener('click', function (e){
-//     console.log("ya kinkin????");
-//     window.location.href="../index.html";
-//   });
+  btn_deconnexion.addEventListener('click', function (e){
+    console.log("ya kinkin????");
+    window.location.href="../index.html";
+  });
 
   // chartJS **********************************
 
--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-var btn_sign= document.getElementById('btn-signIn');
-
-var modal_sign_up=      document.getElementById('modal-signUp');
-var modal_sign_in=      document.getElementById('modal-signIn');
-var btn_inscription=    document.getElementById('btn-inscription');
-var btn_retour=         Array.from(document.getElementsByClassName('btn-retour'));
-var btn_submit=         Array.from(document.getElementsByClassName('btn-submit'));
-var btn_deconnexion=    document.getElementById('btn-deconnexion');
+var btn_sign=           document.getElementById('btn-signIn')
+  , btn_inscription=    document.getElementById('btn-inscription')
+  , btn_deconnexion=    document.getElementById('btn-deconnexion')
+  , modal_sign_up=      document.getElementById('modal-signUp')
+  , modal_sign_in=      document.getElementById('modal-signIn')
+  , btn_retour=         Array.from(document.getElementsByClassName('btn-retour'))
+  , btn_submit=         Array.from(document.getElementsByClassName('btn-submit'))
+  , index_content=      document.getElementById('content');
 
 btn_sign.addEventListener('click',  function () {
     modal_sign_in.classList.toggle('hidepop');
     if (!modal_sign_up.classList.contains('hidepop')) {
         modal_sign_up.classList.add('hidepop');    
     }
+    index_content.classList.toggle('hide');
 });
 
 btn_inscription.addEventListener('click', function () {
